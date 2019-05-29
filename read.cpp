444acc1019069c4ffea136a5a86808dda4d3c494
ksaid0203 <ksaid0203@gmail.com>
2019-05-29 22:50:34 +0900

    read cpp 4th

--- read.cpp
+++ read.cpp
@@ -6,74 +6,107 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <queue>
 using namespace std;
 #define endl '\n'
 
+/**
+ * 버킷정렬의 한 부분
+ * @param arr bucket들
+ * @param i 내가 관심있는 index
+ */
+void radix(vector<queue<string> > & arr, const int i) {
+    vector<int> cnt;
+    for(int j = 0 ; j < 6 ; ++j) {
+        cnt.emplace_back(arr[j].size());
+    }
+    for(int j = 0 ; j < 6 ; j += 1) {
+        for(int k = 0 ; k < cnt[j] ; k += 1) {
+            auto tmp = arr[j].front();
+            if(tmp.size() <= i) {
+                arr[0].emplace(tmp);
+            }
+            else if(tmp[i] == 'a') {
+                arr[1].emplace(tmp);
+            }
+            else if(tmp[i] == 'c') {
+                arr[2].emplace(tmp);
+            }
+            else if(tmp[i] == 'g') {
+                arr[3].emplace(tmp);
+            }
+            else if(tmp[i] == 'n') {
+                arr[4].emplace(tmp);
+            }
+            else if(tmp[i] == 't') {
+                arr[5].emplace(tmp);
+            }
+            arr[j].pop();
+        }
+    }
+}
+
+/**
+ * bucket 내용을 vector로 전달합니다. 그냥 버킷 채로 해도 되는데, 앞전에 구현해 놓은 함수가 vector를 인자로 해서
+ * vector 형태로 합니다.
+ * @param arr bucket들
+ * @param here 데이터를 전달받을 vector
+ */
+void queue2vector(vector<queue<string> > & arr, vector<string> & here) {
+    for(auto x : arr) {
+        while(!x.empty()) {
+            here.emplace_back(x.front());
+            x.pop();
+        }
+    }
+}
+
+/**
+ * 최빈값을 반환
+ * @param here 정렬된 배열
+ * @return 정렬 결과의 최빈값
+ */
+string solve(const vector<string> & here) {
+    string ans;
+    int cnt1 = 0, cnt2 = 1;
+    for(auto it = here.begin() + 1 ; it != here.end(); ++it) {
+        auto it2 = it - 1;
+        if(*it2 == *it) {
+            ++cnt2;
+        }
+        else {
+            if(cnt1 < cnt2) {
+                cnt1 = cnt2;
+                ans = *it2;
+            }
+            cnt2 = 1;
+        }
+    }
+    return ans;
+}
+
 int main() {
-    vector<string> arr;
+    vector<queue<string>> arr(6, queue<string>());
+    vector<string> here;
     string str;
+
     ifstream inp("read.inp");
     int maxLen = 0;
     while(inp >> str) {
         maxLen = max(maxLen, (int)str.size());
-        arr.emplace_back(str);
+        arr[0].emplace(str);
     }
     inp.close();
-    vector<vector<string>> radix;
-    for(int i = maxLen - 1; i >= 0 ; --i) {
-        radix.clear();
-        radix.resize(5, vector<string>());
-        for(const string x : arr) {
-            if(x.size() <= i) {
-                radix[0].emplace_back(x);
-            }
-            else {
-                if(x[i] == 'a') {
-                    radix[1].emplace_back(x);
-                }
-                else if(x[i] == 'c') {
-                    radix[2].emplace_back(x);
-                }
-                else if(x[i] == 'g') {
-                    radix[3].emplace_back(x);
-                }
-                else if(x[i] == 't') {
-                    radix[4].emplace_back(x);
-                }
-            }
-        }
 
-        arr.clear();
-        arr.insert(arr.begin(), radix[0].begin(), radix[0].end());
-        arr.insert(arr.begin() + radix[0].size(), radix[1].begin(), radix[1].end());
-        arr.insert(arr.begin() + radix[0].size() + radix[1].size(), radix[2].begin(), radix[2].end());
-        arr.insert(arr.begin() + radix[0].size() + radix[1].size() + radix[2].size(), radix[3].begin(), radix[3].end());
-        arr.insert(arr.begin() + radix[0].size() + radix[1].size() + radix[2].size() + radix[3].size(), radix[4].begin(), radix[4].end());
+    for(int i = maxLen - 1 ; i >= 0 ; i -= 1) {
+        radix(arr, i);
     }
-    string cand, ans;
-    int cnt1 = 0, cnt2 = 0;
-    arr.emplace_back("0000");
-    for(const string x : arr) {
-        if(cand.empty()) {
-            cand = x;
-        }
-        if(x == cand) {
-            ++cnt2;
-        }
-        else {
-            //cout << x << ' ' << cnt1 << ' ' << cnt2 << endl;
-            if(cnt1 < cnt2) {
-                cnt1 = cnt2;
-                cnt2 = 1;
-                ans = cand;
-            }
-            cand = x;
-        }
-    }
+
+    queue2vector(arr, here);
+    here.emplace_back("0000");
 
     ofstream out("read.out");
-    out << ans << endl;
+    out << solve(here) << endl;
     out.close();
-//    cout << ans << endl;
     return 0;
 }
